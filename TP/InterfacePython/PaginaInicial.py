e5a7635f79c581ba42781e9336d2ffdc09b9ac6e
Felipao98 <80127519+Felipao98@users.noreply.github.com>
2023-08-24 22:58:09 -0300

    Add files via upload

--- TP/InterfacePython/PaginaInicial.py
+++ TP/InterfacePython/PaginaInicial.py
@@ -1,33 +1,55 @@
-import customtkinter
+import customtkinter as CTk
+from tkinter import *
 
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
+CTk.set_appearance_mode("dark")
+CTk.set_default_color_theme("dark-blue")
 
-janela = customtkinter.CTk()
+janela = CTk.CTk()
 janela.title("Página inicial")
 janela.geometry("800x600")
+
+class Table:
+    def __init__(self, root):
+        for i in range(total_rows):
+            for j in range(total_columns):
+                self.e = Entry(root, width=20, fg='blue', font=('Arial', 16, 'bold'))
+                self.e.grid(row=i, column=j)
+                self.e.insert(END, lst[i][j])
+
+lst = [
+    (1, 'ES', 'Obrigatório', 60),
+    (2, 'Mineração de Dados', 'Optativa', 30),
+    (3, 'Pesquisa Operacional', 'Obrigatória', 60),
+    (4, 'Aprendizado de Máquina', 'Optativa', 30),
+    (5, 'CLP', 'Obrigatória', 30)
+]
+
+total_rows = len(lst)
+total_columns = len(lst[0])
 
 def Pessoaclique():
     print("Informações na página principal")
 
 def Gradeclique():
     print("Grade horária")
-    
+    Table(janela)
+
 def Materiaclique():
     print("Matérias ofertadas")
 
 def Historicoclique():
-    print("Historico")
-    
-botaoPessoa = customtkinter.CTkButton(janela, text="Informações pessoais", command=Pessoaclique)
-botaoPessoa.pack(padx=10, pady=10)
+    print("Histórico")
 
-botaoMateria = customtkinter.CTkButton(janela, text="Matérias ofertadas", command=Materiaclique)
-botaoMateria.pack(padx=10, pady=10)
+botaoPessoa = CTk.CTkButton(janela, text="Informações pessoais", command=Pessoaclique)
+botaoPessoa.grid(padx=10, pady=10, sticky=NSEW) #????, padx = distancia do canto da tela, pady = distancia entre os botões https://www.pythontutorial.net/tkinter/tkinter-grid/
 
-botaoHistorico = customtkinter.CTkButton(janela, text="Histórico", command=Historicoclique)
-botaoHistorico.pack(padx=10, pady=10)
+botaoMateria = CTk.CTkButton(janela, text="Matérias ofertadas", command=Materiaclique)
+botaoMateria.grid(padx=10, pady=10)
 
+botaoHistorico = CTk.CTkButton(janela, text="Histórico", command=Historicoclique)
+botaoHistorico.grid(padx=10, pady=10)
 
+botaoGrHoraria = CTk.CTkButton(janela, text="Grade Horária", command=Gradeclique)
+botaoGrHoraria.grid(padx=10, pady=10)
 
 janela.mainloop()

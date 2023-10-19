7e7824346705b47a5d684b9f196225e1aefe7226
Felipao98 <80127519+Felipao98@users.noreply.github.com>
2023-10-19 09:45:33 -0300

    Add files via upload

--- TP/InterfacePython/Tp1/Visao/loginCadastro.py
+++ TP/InterfacePython/Tp1/Visao/loginCadastro.py
@@ -9,32 +9,10 @@ ct.set_default_color_theme("dark-blue")
 #Visão
 
 global Nome, Matricula, Senha
+global Nome_materia, Id, Pre
+
 
 def abrir_janela():
-
-    janela2 = ct.CTkToplevel()
-    janela2.title("Tela de cadastro")
-    janela2.geometry("800x500")
-    
-    
-
-    botao_voltar = ct.CTkButton(janela2, text="Retorna para tela de login", command=janela2.destroy)
-    botao_voltar.pack(padx=10, pady=10)
-    
-    textoC = ct.CTkLabel(janela2, text="Cadastrar usuário")
-    textoC.pack(padx=10, pady=10)
-
-    nomeC = ct.CTkEntry(janela2, placeholder_text="Entre com nome:")
-    nomeC.pack(padx=10, pady=10)
-
-    matriculaC = ct.CTkEntry(janela2, placeholder_text="Entre com a matrícula:")
-    matriculaC.pack(padx=10, pady=10)
-
-    senhaCad = ct.CTkEntry(janela2, placeholder_text="Digite uma senha:", show="*")
-    senhaCad.pack(padx=10, pady=10)
-
-    confirma_senhaC = ct.CTkEntry(janela2, placeholder_text="Confirme sua senha:", show="*")
-    confirma_senhaC.pack(padx=10, pady=10)
 
     def RegisterDataBase():
         
@@ -54,68 +32,98 @@ def abrir_janela():
         
         control.inserir_pessoa(cursor, (Nome, Matricula, Senha))
 
-        cursor.execute("SELECT * FROM MATERIAS")
-        dados = cursor.fetchall()
-
-        cursor.execute("SELECT * FROM HISTORICO")
-        dados_historico = cursor.fetchall()
-
-
         conn.commit()
         conn.close()
 
-        return dados
-        return dados_historico
-
         janela2.destroy()
+
+    
+    janela2 = ct.CTkToplevel()
+    janela2.title("Tela de cadastro")
+    janela2.geometry("800x500")
+    
+    botao_voltar = ct.CTkButton(janela2, text="Retorna para tela de login", command=janela2.destroy)
+    botao_voltar.pack(padx=10, pady=10)
+    
+    textoC = ct.CTkLabel(janela2, text="Cadastrar usuário")
+    textoC.pack(padx=10, pady=10)
+
+    nomeC = ct.CTkEntry(janela2, placeholder_text="Entre com nome:")
+    nomeC.pack(padx=10, pady=10)
+
+    matriculaC = ct.CTkEntry(janela2, placeholder_text="Entre com a matrícula:")
+    matriculaC.pack(padx=10, pady=10)
+
+    senhaCad = ct.CTkEntry(janela2, placeholder_text="Digite uma senha:", show="*")
+    senhaCad.pack(padx=10, pady=10)
+
+    confirma_senhaC = ct.CTkEntry(janela2, placeholder_text="Confirme sua senha:", show="*")
+    confirma_senhaC.pack(padx=10, pady=10)
+
     botao = ct.CTkButton(janela2, text="Finalizar", command=RegisterDataBase)
     botao.pack(padx=10, pady=10)
 
-def concluir_materia():
-    # Lógica para concluir uma matéria
+def chamar_dados(treeview):
+        
+    conn = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Silvafodao98!",
+        database = "Software_APS"
+    )
+
+    cursor = conn.cursor()
+        
+    cursor.execute("SELECT * FROM MATERIAS")
+    dados = cursor.fetchall()
+
+    for record in treeview.get_children():
+        treeview.delete(record)
+        
+    for dado in dados:
+        treeview.insert("", "end", values=dado)
+
+    conn.commit()
+    conn.close()
+
+def criar_tabela():    
+
     janela_tabela = ct.CTkToplevel()
     janela_tabela.title("Tabela de matérias")
     janela_tabela.geometry("800x500")
 
-    tabela = ct.CTkTabview(janela_tabela, data=dados, headers=["Nome_materia", "ID", "Pre"])
-    tabela.pack(padx=10, pady=10)
+    treeview = ct.CTkTreeview(janela_tabela, columns=("Nome_materia", "Id", "Pre"))
+    treeview.heading('#1', text="Nome da materia")
+    treeview.heading('#2', text="Id")
+    treeview.heading('#3', text="Pre")   
+    treeview.pack(padx=10, pady=10)
+
+    chamar_dados(treeview)
 
     print("Matéria concluída!")
 
+    janela_tabela.destroy()
     janela_tabela.mainloop()
 
-
-def ver_historico():
-    # Lógica para ver o histórico do aluno
-    janela_historico = ct.CTkToplevel()
-    janela_historico.title("Historico do aluno")
-    janela_historico.geometry("800x500")
-
-    histórico = ct.CTkTabview(janela_historico, dataH=dados_historico, headers=["ID_aluno", "ID_materia"])
-    histórico.pack(padx=10, pady=10)
-
-    print("Historico do aluno!")
-
-    janela_historico.mainloop()
-
-
-def gerar_grade():
-    # Lógica para gerar a grade de disciplinas
-    janela_grade = ct.CTkToplevel()
-    janela_grade.title("Grade horaria")
-    janela_grade.geometry("800x500")
-
-    grade = ct.CTkTabview(janela_grade, data=dados, headers=["Nome_materia", "ID", "Pre"])
-    tabela.pack(padx=10, pady=10)
-
-    print("Grade gerada!")
-
-    janela_grade.mainloop()
-
 def remover_cadastro():
-    # Lógica para remover o cadastro do aluno
-    print("Cadastro removido!")
+    janela_remove = ct.CTkToplevel()
+    janela_remove.title("Remover cadastro")
+    janela_remove.geometry("800x500")
 
+    tree = ct.CTkTabview(janela_remove, selectmode="browse", column=("column1", "column2", "column3"), show='headings')
+
+    tree.column("column1", width=200, minwidth=50)
+    tree.heading('#1', text="Materia")
+
+    tree.column("column2", width=100, minwidth=50)
+    tree.heading('#2', text="Pre-requisito")
+
+    tree.column("column3", width=300, minwidth=50)
+    tree.heading('#3', text="ID_materia")
+
+    tree.pack(row=0, column=0)
+
+    janela_remove.mainloop()
 
 def cliqueL():
     print("Login com sucesso!")
@@ -123,7 +131,7 @@ def cliqueL():
     janela3.title("Menu principal")
     janela3.geometry("800x500")
 
-    botaoConcluir = ct.CTkButton(janela3, text="Concluir materia", command=concluir_materia)
+    botaoConcluir = ct.CTkButton(janela3, text="Solicitar a disciplina", command=criar_tabela)
     botaoConcluir.pack(padx=10, pady=10)
 
     botaoHistorico = ct.CTkButton(janela3, text="Ver histórico")
@@ -132,14 +140,13 @@ def cliqueL():
     botaoGrade = ct.CTkButton(janela3, text="Gerar grade")
     botaoGrade.pack(padx=10, pady=10)
 
-    botaoRemover = ct.CTkButton(janela3, text="Remover cadastro")
+    botaoRemover = ct.CTkButton(janela3, text="Remover cadastro", command=remover_cadastro)
     botaoRemover.pack(padx=10, pady=10)
 
-    botaoLogout = ct.CTkButton(janela3, text="Logout")
+    botaoLogout = ct.CTkButton(janela3, text="Logout", command=janela)
     botaoLogout.pack(padx=10, pady=10)
 
-
-
+    janela3.destroy()
     janela3.mainloop()
 
 
@@ -169,4 +176,3 @@ botaoC.pack(padx=10, pady=10)
 
 
 janela.mainloop()
-
